6eafaac7de825b0f61f5c8d9d0a2b1c18fada13d
RichardQiu <Richardyuhua@outlook.com>
2018-07-10 18:53:30 +0800

    initialize project
    initialize project

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Schema;
+
+namespace webhookSample.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/message")]
+        public Activity Post([FromBody]Activity request)
+        {
+            return new Activity()
+            {
+                Text = $"you send message is {request.Text}"
+            };
+        }
+    }
+}
